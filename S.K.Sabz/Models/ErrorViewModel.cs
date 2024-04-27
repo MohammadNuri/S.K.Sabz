812dff4c6bde0a9cf074707f6184ee087a64558d
Mohammad Noori <144573411+MohammadNuri@users.noreply.github.com>
2024-04-27 21:57:45 +0330

    Add project files.

---
+++ S.K.Sabz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace S.K.Sabz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
