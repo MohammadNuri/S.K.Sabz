32da0dc7c6436df07eb528e18c5a04cea3f92cfe
Mohammad Noori <144573411+MohammadNuri@users.noreply.github.com>
2024-04-28 18:13:22 +0330

    Contact Page

---
+++ S.K.Sabz/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace S.K.Sabz.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult One()
+        {
+            return View();
+        }
+
+    }
+}
