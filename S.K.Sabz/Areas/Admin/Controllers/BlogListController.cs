0dbf640770c5d1b8d4ef84d8c7c69aa2e1b453bc
Mohammad Noori <144573411+MohammadNuri@users.noreply.github.com>
2024-05-04 23:09:19 +0330

    Add New Post + Post List + BlogList Fornt

---
+++ S.K.Sabz/Areas/Admin/Controllers/BlogListController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace S.K.Sabz.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+
+        public IActionResult AddNewPost()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ S.K.Sabz/Controllers/BlogListController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace S.K.Sabz.Controllers
+{
+    public class BlogListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+        public IActionResult Detail()
+        {
+            return View();
+        }
+
+    }
+}
